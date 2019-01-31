a4a8c94ed393d445f0aa14cabc6cf6c372a819ef
Tedd Mason <teddmason@gmail.com>
2019-01-31 08:28:12 +0000

    setup.sql£

---
+++ fws-setup.sql
@@ -0,0 +1,6 @@
+﻿-- FWS database setup
+
+CREATE TABLESPACE fws_tables OWNER u_fws LOCATION '/fws_tables';
+CREATE TABLESPACE fws_indexes OWNER u_fws LOCATION '/fws_indexes';
+
+CREATE SCHEMA u_fws AUTHORIZATION u_fws;
