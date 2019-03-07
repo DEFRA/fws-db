535ed35684b2f728a9a17c35901160fce208f998
Tedd Mason <teddmason@gmail.com>
2019-03-07 08:05:25 +0000

    renamin fk

--- u_fws/0.0.1/fk_tacode_message_target_area.sql
+++ u_fws/0.0.1/fk_tacode_message_target_area.sql
@@ -3,7 +3,7 @@
 -- ALTER TABLE u_fws.message DROP CONSTRAINT ta_code_fk;
 
 ALTER TABLE u_fws.message
-    ADD CONSTRAINT ta_code_fk FOREIGN KEY (target_area_code)
+    ADD CONSTRAINT fk_tacode_message_target_area FOREIGN KEY (target_area_code)
     REFERENCES u_fws.target_area (ta_code) MATCH SIMPLE
     ON UPDATE NO ACTION
     ON DELETE NO ACTION;
