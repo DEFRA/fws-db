653c7cc1901846c87a2e075a7a06e4ed03d64432
Sameer Srivastava <sameer4414@gmail.com>
2020-03-26 10:38:41 +0000

    FIS-471: FWIS Historic Data Migration related changes

--- u_fws/0.0.3/fis-471-B.sql
+++ u_fws/0.0.3/fis-471-B.sql
@@ -1,1 +1,5 @@
--- TODO : 2 more SQL copy statements need to go here
+-- for psql
+copy u_fws.message(target_area_code,severity,severity_value,situation,situation_changed,severity_changed,message_received,latest,created_by_id,created_by_email,created_by_name)
+FROM '<full path to transformed file created in step 3>' DELIMITER ',' CSV HEADER;
+
+-- TODO : check is pgadmin copy command needs to go in here or not
